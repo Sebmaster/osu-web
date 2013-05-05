Sebmaster/osu-web

commit 14e4847d5986cd83f295dbcc66a97968fa1c4145

Let the osu instance manage the canvas/playarea
This allows us to use <img> or <video> tags in the area to enable video
storyboarding.

newjs/OSU.js
@@ -1,6 +1,6 @@
 ﻿function OSU(playArea, songArea) {
 	this.playArea = jQuery(playArea);
-	this.context = this.playArea[0].getContext('2d');
+	this.context = jQuery('<canvas>').appendTo(this.playArea).getContext('2d');
 }
 
 OSU.prototype.bindEvents = function (newSong) {

newjs/init.js
@@ -55,9 +55,13 @@ jQuery(function () {
 			}
 		});
 	}
+	var osu = new OSU('#osu');
+	osu.bindEvents(function () {
+		refreshSongs();
+	});
 
 	jQuery(window).on('resize', function () {
-		var canvas = jQuery('#osu');
+		var canvas = jQuery(osu.context.canvas);
 		var cvWidth = canvas.width();
 		var cvHeight = canvas.height();
 
@@ -65,10 +69,6 @@ jQuery(function () {
 	});
 
 	jQuery(window).trigger('resize');
-	var osu = new OSU('#osu');
-	osu.bindEvents(function () {
-		refreshSongs();
-	});
 
 	refreshSongs();
 });

index.html
@@ -32,6 +32,6 @@
 
 <body>
     <ul id="songs"></ul>
-    <canvas id="osu" style="display: none"></canvas>
+    <div id="osu" style="display: none"></div>
 </body>
 </html>

commit a58d714a32760ceddaf1440f54c0885fdcb320a5

Fix bugs introduced in previous commit
Commit 14e4847d5986cd83f295dbcc66a97968fa1c4145.

newjs/OSU.js
@@ -1,6 +1,6 @@
 ﻿function OSU(playArea, songArea) {
 	this.playArea = jQuery(playArea);
-	this.context = jQuery('<canvas>').appendTo(this.playArea).getContext('2d');
+	this.context = jQuery('<canvas>').css({ width: '100%', height: '100%' }).appendTo(this.playArea)[0].getContext('2d');
 }
 
 OSU.prototype.bindEvents = function (newSong) {

newjs/init.js
@@ -61,11 +61,18 @@ jQuery(function () {
 	});
 
 	jQuery(window).on('resize', function () {
+		var disp = osu.playArea.css('display');
+		if (disp === 'none') {
+			jQuery('#osu').css('display', '');
+		}
 		var canvas = jQuery(osu.context.canvas);
 		var cvWidth = canvas.width();
 		var cvHeight = canvas.height();
 
 		canvas.prop({ width: cvWidth, height: cvHeight });
+		if (disp === 'none') {
+			osu.playArea.css('display', 'none');
+		}
 	});
 
 	jQuery(window).trigger('resize');